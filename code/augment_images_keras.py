8868db6519525797415d62c614d6c259a05f8de0
AdityaShenoy <adityashenoy49@gmail.com>
2020-04-24 12:29:13 +0530

    .

--- code/augment_images_keras.py
+++ code/augment_images_keras.py
@@ -4,50 +4,84 @@ import time
 import tensorflow as tf
 import numpy as np
 from PIL import Image
+
+# Class labels
 CLASSES = 'cdnp'
+
+# Folder paths
 FOLDER_PREFIX = 'F:/github/Drones_For_Structural_Audit/dataset/internal'
-INPUT_FOLDER = f'{FOLDER_PREFIX}/256_raw'
-OUTPUT_FOLDER = f'{FOLDER_PREFIX}/256_split'
-AUGMENTATION_FOLDER = f'{FOLDER_PREFIX}/256_aug'
-if os.path.exists(OUTPUT_FOLDER):
-  shutil.rmtree(OUTPUT_FOLDER, onerror=lambda _: _)
-if os.path.exists(AUGMENTATION_FOLDER):
-  shutil.rmtree(AUGMENTATION_FOLDER, onerror=lambda _: _)
-time.sleep(1)
-os.mkdir(OUTPUT_FOLDER)
-os.mkdir(AUGMENTATION_FOLDER)
-for class_ in CLASSES:
-  os.mkdir(f'{OUTPUT_FOLDER}/{class_}')
-  os.mkdir(f'{AUGMENTATION_FOLDER}/{class_}')
+RAW_FOLDER = f'{FOLDER_PREFIX}/256_raw'
+SPLIT_FOLDER = f'{FOLDER_PREFIX}/256_split'
+NO_DISTORT_AUG_FOLDER = f'{FOLDER_PREFIX}/256_no_dist_aug'
+AUG_FOLDER = f'{FOLDER_PREFIX}/256_aug'
+
+# For all 3 folders
+for folder in [SPLIT_FOLDER, NO_DISTORT_AUG_FOLDER, AUG_FOLDER]:
+
+  # If the folder already exists
+  if os.path.exists(folder):
+    
+    # Delete the folder
+    shutil.rmtree(folder, onerror=lambda _: _)
+
+    # Wait for 1 second
+    time.sleep(1)
+
+    # Make empty folder again
+    os.mkdir(folder)
+
+    # Make class label sub folders
+    for class_ in CLASSES:
+      os.mkdir(f'{folder}/{class_}')
+
+# Initialize count of samples in each class to 0
 num_samples = {class_: 0 for class_ in CLASSES}
-with os.scandir(INPUT_FOLDER) as folder:
+
+# Scan the raw samples folder
+with os.scandir(RAW_FOLDER) as folder:
+
+  # Iterate for all files in the folder
   for file in folder:
+
+    # Extract the label number 0123 from the file name
     label = int(file.name.split('_')[3][0])
+
+    # Increment the count of sample of this class by 1
     num_samples[CLASSES[label]] += 1
-    shutil.copy(src=file.path, dst=f'{OUTPUT_FOLDER}/{CLASSES[label]}')
-aug_ratio = {class_: 10_000 // num_samples[class_] for class_ in CLASSES}
+
+    # Copy the file to label folder inside the split folder
+    shutil.copy(src=file.path, dst=f'{SPLIT_FOLDER}/{CLASSES[label]}')
+
+    # Open this image file
+    img = Image.open(file.path)
+
+    # Save the original image in the no distortion aug folder
+    img.save(f'{NO_DISTORT_AUG_FOLDER}/{CLASSES[label]}/{file.name[:-4]}_0deg.jpg')
+
+    # For all non distorting augmentation operations and their name suffix
+    for op, suffix in {
+                        Image.ROTATE_90: '90deg', Image.ROTATE_180: '180deg',
+                        Image.ROTATE_270: '270deg', Image.FLIP_TOP_BOTTOM: 'verti',
+                        Image.FLIP_LEFT_RIGHT: 'hori', Image.TRANSPOSE: 'transpose',
+                        Image.TRANSVERSE: 'transverse'
+                       }.items():
+      
+      # Perform operation and save the file with proper suffix
+      img.transpose(op)\
+        .save(f'{NO_DISTORT_AUG_FOLDER}/{CLASSES[label]}/{file.name[:-4]}_{suffix}.jpg')
+
+# The augmentation ratio is the factor by which the non distorted images need
+# to be augmented to get the input data close to 10,000 samples
+aug_ratio = {class_: 10_000 // (num_samples[class_] * 8) for class_ in CLASSES}
+
+# Keras data generator which performs distorting
+# augmentations as specified in the parameter
 data_gen = tf.keras.preprocessing.image.ImageDataGenerator(
   zca_whitening = True,
-  rotation_range = 40,
+  rotation_range = 89,
   width_shift_range = 0.2,
   height_shift_range = 0.2,
   brightness_range = [0.8, 1.2],
   shear_range = 0.2,
   zoom_range = 0.2,
-  horizontal_flip = True,
-  vertical_flip = True,
 )
-for class_ in CLASSES:
-  with os.scandir(f'{OUTPUT_FOLDER}/{class_}') as folder:
-    for f, file in enumerate(folder, start=1):
-      img = np.asarray(Image.open(file.path)).reshape((1, 256, 256, 3))
-      for _ in data_gen.flow(
-                  img,
-                  save_to_dir=f'{AUGMENTATION_FOLDER}/{class_}',
-                  batch_size=1,
-                  shuffle=False,
-                  save_format='jpeg',
-                  save_prefix=f'{file.name[:-4]}'
-                ):
-        if len(os.listdir(f'{AUGMENTATION_FOLDER}/{class_}')) == aug_ratio[class_]*f:
-          break
--- code/generate_samples_256_manual.py
+++ code/generate_samples_256_manual.py
@@ -1,55 +1,115 @@
 from PIL import Image
 from re import match
 import pyautogui as pag
+
+# Folder paths
 FOLDER_PREFIX = 'F:/github/Drones_For_Structural_Audit/dataset'
 INPUT_FOLDER = f'{FOLDER_PREFIX}/raw/all_images'
 OUTPUT_FOLDER = f'{FOLDER_PREFIX}/internal/256'
+
+# Image count
 INPUT_IMG_CNT = 228
+
+# Start the program from this image
 IMG_START = 5
+
+# Image dimension
 IMG_SIZE = 256
+
+# Grid for red border
 GRID = Image.open('F:/github/Drones_For_Structural_Audit/images/grid.png').convert('RGBA')
+
+# Iterate for all images starting from IMG_START
 for img_num in range(IMG_START, INPUT_IMG_CNT):
+
+  # Open the image
   img = Image.open(f'{INPUT_FOLDER}/{img_num:03}.jpg')
+
+  # Extract width and height of the image
   w, h = img.width, img.height
+
+  # Visited set to avoid overlap of 2 samples
   vis = set()
+
+  # Height loop
   y = 0
   while y < h - IMG_SIZE + 1:
+
+    # Width loop
     x = 0
     while x < (w - IMG_SIZE + 1):
+
+      # Check if the IMG_SIZE window is already visited
       for xx in range(IMG_SIZE):
         if (x + xx, y) in vis:
           break
+      
+      # If not visited
       else:
+
+        # Print progress
         print(f'Processing {(x, y)}')
+
+        # Crop the image in the window
         cropped_img = img.crop((x, y, x + IMG_SIZE, y + IMG_SIZE))
+
+        # Scale the image to original image height
         scaled_img = cropped_img.resize((h, h))
+
+        # Create a new image for showing original image and above scaled image
         img_to_show = Image.new(mode='RGB', size=(w+h, h))
         img_to_show.paste(img, box=(0, 0))
         img_to_show.paste(scaled_img, box=(w, 0))
+
+        # Highlight the window with red border grid
         img_to_show.paste(GRID, box=(x, y), mask=GRID)
+
+        # Show user the image
         img_to_show.show()
+
+        # Ask for input
         label = input(f'Enter label (cdnp / 1-{w - IMG_SIZE - x} / x / xx / y1-y{h - IMG_SIZE - y}): ')
+
+        # Change the application tab and close the image
         pag.hotkey('alt', 'tab')
         pag.hotkey('alt', 'f4')
+
+        # If image is labelled
         if label in 'cdnp':
+
+          # Convert cdnp to 0123
           l = 'cdnp'.find(label)
+
+          # Save the sample in the output folder with proper name
           cropped_img.save(f'{OUTPUT_FOLDER}/{img_num:03}_{x:04}_{y:04}_{l}.jpg')
+
+          # Mark the window visited
           for yy in range(IMG_SIZE):
             for xx in range(IMG_SIZE):
               vis.add((x + xx, y + yy))
+        
+        # Discard the sample and add it to visited set
         elif label == 'x':
           for yy in range(IMG_SIZE):
             for xx in range(IMG_SIZE):
               vis.add((x + xx, y + yy))
+        
+        # Discard the whole row and add it to visited set
         elif label == 'xx':
           for yy in range(IMG_SIZE):
             for xx in range(w - x + 1):
               vis.add((x + xx, y + yy))
+        
+        # If input is a number the increment the x value by that value
         elif match('\\d+', label):
           x += int(label) - 1
+        
+        # If input is y and a number the increment the y value by that value
         elif match('y\\d+', label):
           y += int(label[1:]) - 1
           break
+          
+        # If invalid input, do not do anything
         else:
           x -= 1
       x += 1
