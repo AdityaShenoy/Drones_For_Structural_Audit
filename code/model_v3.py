41f37965785f494d008af701d0402f2a3eb75d0c
AdityaShenoy <adityashenoy49@gmail.com>
2020-04-25 11:05:54 +0530

    .

--- code/model_v3.py
+++ code/model_v3.py
@@ -16,6 +16,7 @@ BATCH_SIZE = 32
 IMG_SIZE = 256
 CLASSES = 'cdnp'
 MAX_HIDDEN_LAYERS = 2
+MAX_NODES_PER_LAYER = 256
 
 # Folder paths
 # FOLDER_PREFIX = f'F:/github/Drones_For_Structural_Audit/dataset/internal/{IMG_SIZE}'
@@ -64,6 +65,7 @@ def gen(var, mode):
             # Reset the batch again
             res = []
 
+# Set the output for printing to a file
 sys.stdout = open(f'{FOLDER_PREFIX}/model_summary.txt', 'w')
 
 # Architecture of the neural network
@@ -72,6 +74,7 @@ hidden_nodes = [1]
 # While the hidden layers are less than the 
 while len(hidden_nodes) <= MAX_HIDDEN_LAYERS:
 
+  # Print progress
   print('='*100)
   print('Current NN architecture:')
   print(*hidden_nodes, sep=' → ')
@@ -101,7 +104,7 @@ while len(hidden_nodes) <= MAX_HIDDEN_LAYERS:
     x = gen(var='x', mode='train'),
     y = gen(var='y', mode='train'),
     epochs = 20,
-    verbose = 1,
+    verbose = 2,
     steps_per_epoch = TRAINING_SIZE // BATCH_SIZE
   )
 
@@ -109,14 +112,14 @@ while len(hidden_nodes) <= MAX_HIDDEN_LAYERS:
   model.evaluate(
     x = gen(var='x', mode='test'),
     y = gen(var='y', mode='test'),
-    verbose = 1,
+    verbose = 2,
     steps_per_epoch = TESTING_SIZE // BATCH_SIZE
   )
 
   # Reconfigure the architecture
-  if hidden_nodes[-1] == IMG_SIZE:
+  if hidden_nodes[-1] == MAX_NODES_PER_LAYER:
     for i in range(-1, -len(hidden_nodes)-1, -1):
-      if hidden_nodes[i] == IMG_SIZE:
+      if hidden_nodes[i] == MAX_NODES_PER_LAYER:
         hidden_nodes[i] = 1
       else:
         hidden_nodes[i] *= 2
@@ -126,13 +129,15 @@ while len(hidden_nodes) <= MAX_HIDDEN_LAYERS:
   else:
     hidden_nodes[-1] *= 2
   
+  # Print progress
   print('='*100)
 
-# Note end time
+# Note ending time
 end = time.time()
 
 # Calculate time difference
 exec_time = int(end - start)
 
+# Print execution time
 print(f'Total execution time: {exec_time}s (' + \
       f'{exec_time // 3600}h {(exec_time // 60) % 60}m {exec_time % 60}s)')
